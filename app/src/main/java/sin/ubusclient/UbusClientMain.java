252112e863fa5d8f7d495f0e21d490eb0b441a85
Sin <346490349@qq.com>
2017-06-06 17:39:47 +0800

    First commit.

---
+++ app/src/main/java/sin/ubusclient/UbusClientMain.java
@@ -0,0 +1,13 @@
+package sin.ubusclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UbusClientMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ubus_client_layout);
+    }
+}
